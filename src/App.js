205b6628da09cdc04191e2b5078fe66ba4b6440c
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-22 16:38:52 -0700

    App changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import employees from './employees.json'
+import EmployeeTable from './components/EmployeeTable'
+import SearchForm from './components/SearchForm'
+import EmpJumbotron from './components/EmpJumbotron'
 
 
+class App extends Component {
+//setting this.state.employees to the employees json array
+  state = {
+    employees,
+    originalEmployees: employees
+  }
+
+//handleSort()
+
+  // handleFilter(e){
+  //   let input = e.target.value
+
+  //   this.setState({
+  //     employees: originalEmployees.filter(emp => emp.name.iindexOf(input) !== -1)
+  //   })
+    // filter original employees using the input updating employees
+
+  // }
+
+  render(){
+    return (
+      <>
+        <EmpJumbotron></EmpJumbotron>
+        <SearchForm></SearchForm>
+        <EmployeeTable></EmployeeTable>
+      </>
+
+    )
+  }
+
+}
 
 export default App;
