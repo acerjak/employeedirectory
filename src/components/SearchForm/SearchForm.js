9e09d9c3d68dbc9ff0b2a772fc1997b512b5acb5
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-22 23:13:20 -0700

    added form component back in

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Form, FormGroup, Label, Input } from 'reactstrap'
 
 
-const SearchForm = () => {
+const SearchForm = props => {
     return (
       <Form inline>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label for="name" className="mr-sm-2">Search:</Label>
-        <Input type="name" name="name" id="name" placeholder="start typing name" />
+        <Input 
+        type="name" 
+        title="first_name" 
+        name="first_name" 
+        id="first_name" 
+        placeholder="start typing name"
+        onChange={props.handleInputChange}
+        value={props.first_name} />
       </FormGroup>
-      <Button>Submit</Button>
     </Form>
     )
 }
