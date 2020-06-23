a810c60df527e61f77ca3c8421638cfcc05aa4ff
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-22 17:57:02 -0700

    handing props to table

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ import EmpJumbotron from './components/EmpJumbotron'
 class App extends Component {
 //setting this.state.employees to the employees json array
   state = {
+    firstName: '',
+    lastName: '',
     employees,
     originalEmployees: employees
   }
 
+//handle input change
+handleInputChange = event => {
+  event.preventDefault()
+  this.setState({ [event.target.name]: event.target.value })
+}
+
+//handle filter by name
+// handleFilter = event => {
+//   event.preventDefault()
+//   const employees = this.setState({ employees: employees.filter(emp => emp.name.indexOf(input) !== -1)})
+// }
 //handleSort()
 
   // handleFilter(e){
@@ -29,8 +42,23 @@ class App extends Component {
     return (
       <>
         <EmpJumbotron></EmpJumbotron>
-        <SearchForm></SearchForm>
-        <EmployeeTable></EmployeeTable>
+        <SearchForm
+        onChange={this.handleInputChange}
+        value={this.state.firstName}
+        ></SearchForm>
+        {this.state.employees.map(employee => (
+          <EmployeeTable
+          id={employee.id}
+          key={employee.id}
+          first_name={employee.first_name}
+          last_name={employee.last_name}
+          email={employee.email}
+          job_title={employee.job_title}
+          department={employee.department}
+          phone={employee.phone}
+          />
+        ))}
+        
       </>
 
     )
