2e50ca591ba3cfd102defb2a03ef1dc2852d5246
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-22 17:56:21 -0700

    bring props into employee table

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table'
 
-const EmployeeTable = () => {
+const EmployeeTable = props => {
     return (
         <Table striped bordered hover variant="dark">
           <thead>
           <tr>
-            <th>Image</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
             <th>Job Title</th>
+            <th>Department</th>
             <th>Phone</th>
           </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td>{props.first_name}</td>
+              <td>{props.last_name}</td>
+              <td>{props.email}</td>
+              <td>{props.job_title}</td>
+              <td>{props.department}</td>
+              <td>{props.phone}</td>
             </tr>
           </tbody>
         </Table>
